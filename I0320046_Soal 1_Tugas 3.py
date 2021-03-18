7efb50320ced65c3c0ea40ecc03a8d9b7bee5f5b
Hasanravi03 <80537664+Hasanravi03@users.noreply.github.com>
2021-03-18 14:42:39 +0700

    Add files via upload

---
+++ I0320046_Soal 1_Tugas 3.py
@@ -0,0 +1,20 @@
+# List 10 teman
+list = ['Hafizh','Fagih','Evelyn','Alung','Fatih','Misel','Kiara','Mario','Aurel','Rafli']
+
+# Menampilkan list indeks nomor 4,6, dan 7
+print("List indeks nomor 4,6, dan 7 adalah",list[4],",",list[6],", dan",list[7])
+
+# Mengganti nama teman di list 3,5, dan 9
+list[3] = 'Lutfi'
+list[5] = 'Arya'
+list[9] = 'Vivi'
+
+# Menambahkan 2 nama teman
+list.append('Tyo')
+list.append('Farras')
+
+# Menampilkan semua teman dengan pengulangan
+print(list*2)
+
+# Menampilkan panjang list
+print(len(list))
---
+++ I0320046_Soal 2_Tugas 3.py
@@ -0,0 +1,9 @@
+# Membuat dictionary
+dict = {'Nama':'Hasan Ravi','Hobi1':'traveling','Hobi2':'Futsal','Hobi3':'','Sosmed1':'ig:@hasanravir','Sosmed2':'ig:@panggilsajasantuy','Sosmed3':'line:hasanravir','Lagu1':'langit abu-abu','Lagu2':'bed of rose','Lagu3':'iris','Makanan1':'mac n cheese','Makanan2':'kwetiaw goreng','Makanan3':'steak sapi'}
+
+# Mengubah salah satu hobi dan sosmed, hapus 2 makanan, dan tambah 1 hobi
+dict['Hobi2'] = 'berenang'
+dict['Sosmed3'] = 'facebook:hasan ravi'
+del dict['Makanan3']
+del dict['Makanan2']
+dict['Hobi4'] = 'jalan jalan'
